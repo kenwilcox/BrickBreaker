1945b78a26b2c471d71df01ff4142661900c73c3
kenwilcox <wilcoxk@gmail.com>
2014-10-20 11:06:22 -0600

    Setup Project

--- BrickBreaker/BBViewController.m
+++ BrickBreaker/BBViewController.m
@@ -13,39 +13,44 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+  [super viewDidLoad];
+  
+  // Configure the view.
+  SKView * skView = (SKView *)self.view;
+  skView.showsFPS = YES;
+  skView.showsNodeCount = YES;
+  
+  // Create and configure the scene.
+  SKScene * scene = [BBMyScene sceneWithSize:skView.bounds.size];
+  scene.scaleMode = SKSceneScaleModeAspectFill;
+  
+  // Present the scene.
+  [skView presentScene:scene];
+}
 
-    // Configure the view.
-    SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
-    
-    // Create and configure the scene.
-    SKScene * scene = [BBMyScene sceneWithSize:skView.bounds.size];
-    scene.scaleMode = SKSceneScaleModeAspectFill;
-    
-    // Present the scene.
-    [skView presentScene:scene];
+- (BOOL)prefersStatusBarHidden
+{
+  return YES;
 }
 
 - (BOOL)shouldAutorotate
 {
-    return YES;
+  return YES;
 }
 
 - (NSUInteger)supportedInterfaceOrientations
 {
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return UIInterfaceOrientationMaskAllButUpsideDown;
-    } else {
-        return UIInterfaceOrientationMaskAll;
-    }
+  if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+    return UIInterfaceOrientationMaskAllButUpsideDown;
+  } else {
+    return UIInterfaceOrientationMaskAll;
+  }
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-    // Release any cached data, images, etc that aren't in use.
+  [super didReceiveMemoryWarning];
+  // Release any cached data, images, etc that aren't in use.
 }
 
 @end
