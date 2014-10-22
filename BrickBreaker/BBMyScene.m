16fdcc9a38a156f26d38ea1e1ce846f1d4db085e
kenwilcox <wilcoxk@gmail.com>
2014-10-21 19:43:22 -0600

    Limit paddle movement

--- BrickBreaker/BBMyScene.m
+++ BrickBreaker/BBMyScene.m
@@ -41,10 +41,21 @@
 
 -(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
   for (UITouch *touch in touches) {
-    // Calculate how far touch has moved on x axis.
+    // Calculate how far touch has moved on x axis
     CGFloat xMovement = [touch locationInNode:self].x - _touchLocation.x;
-    // Move paddle distance of touch.
+    // Move paddle distance of touch
     _paddle.position = CGPointMake(_paddle.position.x + xMovement, _paddle.position.y);
+    
+    CGFloat paddleMinX = -_paddle.size.width * 0.25;
+    CGFloat paddleMaxX = self.size.width + (_paddle.size.width * 0.25);
+    // Cap paddle's position so it remains on screen
+    if (_paddle.position.x < paddleMinX) {
+      _paddle.position = CGPointMake(paddleMinX, _paddle.position.y);
+    }
+    if (_paddle.position.x > paddleMaxX) {
+      _paddle.position = CGPointMake(paddleMaxX, _paddle.position.y);
+    }
+    
     _touchLocation = [touch locationInNode:self];
   }
 }
